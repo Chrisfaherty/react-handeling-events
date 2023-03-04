18801076502596b3df534e300552b57f170611d9
Chrisfaherty <chrismactom@gmail.com>
2023-03-04 13:46:22 +0000

    Create event components

---
+++ my-app/src/components/EventsFunctional.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class EventsFunctional extends Component {
+  render() {
+    return (
+      <div>
+        <button>Click me - functional component</button>
+      </div>
+    )
+  }
+}
+
+export default EventsFunctional
